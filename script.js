6f7cfc1e8499b4bb6c36ad2e77f53371b2da97ec
William Boothe <89033570+WilliamBoothe@users.noreply.github.com>
2024-11-14 15:53:41 -0600

    Add function for computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+  if (randomNumber === 1) {
+    return "Rock";
+  } else if (randomNumber === 2) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
